610b426063469a8419f97f8853a3b28f88b0d40d
Gitesh24398 <narkhedegitesh@gmail.com>
2021-10-19 16:50:01 +0530

    ADDED: User entity

---
+++ src/main/java/com/bank/entity/User.java
@@ -0,0 +1,130 @@
+package com.bank.entity;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="userid")
+	private int userid;
+	
+	@Column(name="firstname")
+	private String firstname;
+	
+	@Column(name="lastname")
+	private String lastname;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="mobileno")
+	private String mobileno;
+	
+	@Column(name="dateOfBirth")
+	private LocalDate dateOfBirth;
+	
+	public User() {}
+
+	public User(int userid, String firstname, String lastname, String username, String password, String email,
+			String mobileno, LocalDate dateOfBirth) {
+		super();
+		this.userid = userid;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.mobileno = mobileno;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobileno() {
+		return mobileno;
+	}
+
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userid=" + userid + ", firstname=" + firstname + ", lastname=" + lastname + ", username="
+				+ username + ", password=" + password + ", email=" + email + ", mobileno=" + mobileno + ", dateOfBirth="
+				+ dateOfBirth + "]";
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/bank/services/UserDetails.java
@@ -0,0 +1,6 @@
+package com.bank.services;
+
+
+public interface UserDetails {
+
+}
